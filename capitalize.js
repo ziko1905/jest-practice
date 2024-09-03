c80f36978bdd7f7765f0fe22b431b685794a7e98
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-03 16:45:33 +0200

    Fix and test capitalize

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,5 @@
-function capitalize () {
+function capitalize (word) {
+    return `${word.charAt(0).toLocaleUpperCase()}${word.slice(1)}`
 }
 
 module.exports = capitalize
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,6 +1,18 @@
 const capitalize = require("./capitalize")
 
 // Testing 
-test("Is recognized", () => {
-    expect(typeof capitalize).toBe("function")
+describe("", () => {
+    it("Is recognized", () => {
+        expect(typeof capitalize).toBe("function")
+    })
 })
+
+const testcases = ["foo", "test", "TEST", "tEST"];
+const answ = ["Foo", "Test", "TEST", "TEST"];
+
+testcases.forEach((val, inx) => {
+    test(`First letter capitalized(${val})`, () =>
+        expect(capitalize(val)).toBe(answ[inx]))
+    
+});
+
